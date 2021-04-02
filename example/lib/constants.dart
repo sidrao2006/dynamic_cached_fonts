9ae60d4e9b001dffc93aa18dcaf7591150380b9d
Aneesh Rao <sidrao2006@gmail.com>
2021-04-03 00:01:23 +0530

    Revert "Use rawcdn.githack to prevent cors errors"
    This reverts commit bee9229c0173bd0194885859c328d2082ab83259.

--- example/lib/constants.dart
+++ example/lib/constants.dart
@@ -12,15 +12,15 @@ const String firaSans = 'FiraSans';
 // Font source urls
 
 const String notoSansUrl =
-    'https://rawcdn.githack.com/googlefonts/noto-fonts/v20201206-phase3/hinted/ttf/NotoSans/NotoSans-Regular.ttf';
+    'https://cdn.jsdelivr.net/gh/googlefonts/noto-fonts@v20201206-phase3/hinted/ttf/NotoSans/NotoSans-Regular.ttf';
 const String cascadiaCodeUrl =
-    'https://rawcdn.githack.com/ryanoasis/nerd-fonts/v2.1.0/patched-fonts/CascadiaCode/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Mono.ttf';
+    'https://cdn.jsdelivr.net/gh/ryanoasis/nerd-fonts@v2.1.0/patched-fonts/CascadiaCode/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Mono.ttf';
 const String firaMonoUrl =
-    'https://rawcdn.githack.com/mozilla/Fira/4.202/ttf/FiraMono-Regular.ttf';
+    'https://cdn.jsdelivr.net/gh/mozilla/Fira@4.202/ttf/FiraMono-Regular.ttf';
 
 const String firaSansBoldUrl =
-        'https://rawcdn.githack.com/mozilla/Fira/4.202/ttf/FiraSans-Bold.ttf',
-    firaSansRegularUrl = 'https://rawcdn.githack.com/mozilla/Fira/4.202/ttf/FiraSans-Regular.ttf',
-    firaSansItalicUrl = 'https://rawcdn.githack.com/mozilla/Fira/4.202/ttf/FiraSans-Italic.ttf',
-    firaSansThinUrl = 'https://rawcdn.githack.com/mozilla/Fira/4.202/ttf/FiraSans-Thin.ttf';
+        'https://cdn.jsdelivr.net/gh/mozilla/Fira@4.202/ttf/FiraSans-Bold.ttf',
+    firaSansRegularUrl = 'https://cdn.jsdelivr.net/gh/mozilla/Fira@4.202/ttf/FiraSans-Regular.ttf',
+    firaSansItalicUrl = 'https://cdn.jsdelivr.net/gh/mozilla/Fira@4.202/ttf/FiraSans-Italic.ttf',
+    firaSansThinUrl = 'https://cdn.jsdelivr.net/gh/mozilla/Fira@4.202/ttf/FiraSans-Thin.ttf';
 const String firaCodeUrl = String.fromEnvironment('FIREBASE_STORAGE_FONT_URL');
