b373d56385fb71694337c866b1597e51f91b6c9e
Aneesh Rao <sidrao2006@gmail.com>
2021-05-23 12:10:41 +0530

    refactor: Use static methods to compose the load method (#47)
    * feat(Testability Enhancements): Add custom fontLoader param to static cache loader methods
    
    - Add `fontLoader` parameter to `loadCachedFont`
    - Add `fontLoader` parameter to `loadCachedFamily`
    - Expose `fontLoader` only for testing
    
    * refactor(`loadCachedFamily`): Add null check to font
    
    - Before:
      * `NoSuchMethodError` was thrown.
      * In a try-catch block, the error thrown won't immediately stop the execution of the function.
    - After: Assertion error with a clear error message is thrown
    
    * refactor: Use static methods to compose the load method
    
    - `DynamicCachedFonts.load` now returns FileInfo instead of File
    - Use `loadCachedFamily` and `cacheFont` to compose the load method
    - Implement font file validity checker to preserve `Cachemanager.getSingleFile`'s behaviour
    
    * build(deps): Remove `file` package

--- lib/src/raw_dynamic_cached_fonts.dart
+++ lib/src/raw_dynamic_cached_fonts.dart
@@ -171,8 +171,11 @@ abstract class RawDynamicCachedFonts {
     String url, {
     @required String fontFamily,
     bool verboseLog = false,
+    @visibleForTesting FontLoader fontLoader,
   }) async {
     assert(verboseLog != null);
+
+    fontLoader ??= FontLoader(fontFamily);
 
     WidgetsFlutterBinding.ensureInitialized();
 
@@ -185,10 +188,9 @@ abstract class RawDynamicCachedFonts {
 
     final ByteData cachedFontBytes = ByteData.view(fontBytes.buffer);
 
-    final FontLoader fontLoader = FontLoader(fontFamily)
-      ..addFont(
-        Future<ByteData>.value(cachedFontBytes),
-      );
+    fontLoader.addFont(
+      Future<ByteData>.value(cachedFontBytes),
+    );
 
     await fontLoader.load();
 
@@ -231,8 +233,11 @@ abstract class RawDynamicCachedFonts {
     List<String> urls, {
     @required String fontFamily,
     bool verboseLog = false,
+    @visibleForTesting FontLoader fontLoader,
   }) async {
     assert(verboseLog != null);
+
+    fontLoader ??= FontLoader(fontFamily);
 
     WidgetsFlutterBinding.ensureInitialized();
 
@@ -247,13 +252,16 @@ abstract class RawDynamicCachedFonts {
       }),
     );
 
+    assert(
+      fontFiles.every((FileInfo font) => font != null),
+      'Font should already be cached to be loaded',
+    );
+
     final Iterable<Future<ByteData>> cachedFontBytes = fontFiles.map((FileInfo font) async {
       final Uint8List fontBytes = await font.file.readAsBytes();
 
       return ByteData.view(fontBytes.buffer);
     });
-
-    final FontLoader fontLoader = FontLoader(fontFamily);
 
     for (final Future<ByteData> bytes in cachedFontBytes) fontLoader.addFont(bytes);
 
