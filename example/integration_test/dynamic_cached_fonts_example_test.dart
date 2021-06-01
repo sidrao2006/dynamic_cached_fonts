690ac5de7af2a763e7cbdd670075b03763201e66
Aneesh Rao <sidrao2006@gmail.com>
2021-06-02 00:15:39 +0530

    Add tests for `loadCachedFont` and `loadCachedFamily`
    Tests added -
    
    - `DynamicCachedFonts.loadCachedFont` loads font into cache
    - `DynamicCachedFonts.loadCachedFont` loads valid font file
    - `DynamicCachedFonts.loadCachedFont` loads font into the Flutter Engine
    - `DynamicCachedFonts.loadCachedFamily` loads font into cache
    - `DynamicCachedFonts.loadCachedFamily` loads valid font files
    - `DynamicCachedFonts.loadCachedFamily` loads font into the Flutter Engine

--- example/integration_test/dynamic_cached_fonts_example_test.dart
+++ example/integration_test/dynamic_cached_fonts_example_test.dart
@@ -2,6 +2,7 @@ import 'package:dynamic_cached_fonts/dynamic_cached_fonts.dart';
 import 'package:dynamic_cached_fonts_example/constants.dart';
 import 'package:firebase_core/firebase_core.dart' show Firebase;
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/services.dart' show FontLoader;
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart' show IntegrationTestWidgetsFlutterBinding;
@@ -171,6 +172,91 @@ void main() {
           false,
         ),
       );
+    });
+  });
+
+  group('DynamicCachedFonts.loadCachedFont', () {
+    FileInfo downloadedFontFile;
+    FileInfo fontFile;
+    FontLoader fontLoader;
+
+    setUpAll(() async {
+      downloadedFontFile = await cacheManager.downloadFile(fontUrl, key: cacheKey);
+
+      fontLoader = FontLoader(fontName);
+
+      fontFile = await DynamicCachedFonts.loadCachedFont(
+        fontUrl,
+        fontFamily: fontName,
+        fontLoader: fontLoader,
+      );
+    });
+
+    testWidgets('loads font into cache', (_) async {
+      expect(fontFile, isNotNull);
+    });
+
+    testWidgets('loads valid font file', (_) async {
+      expect(
+        downloadedFontFile.file.readAsBytesSync(),
+        fontFile.file.readAsBytesSync(),
+      );
+    });
+
+    testWidgets('loads font into the Flutter Engine', (_) async {
+      // This tests that load() is being called in loadCachedFamily.
+      // A single instance of FontLoader can only be loaded once, so if
+      // StateError is thrown, it means that load() has already been called.
+      expect(fontLoader.load(), throwsStateError);
+    });
+  });
+
+  group('DynamicCachedFonts.loadCachedFamily', () {
+    Iterable<FileInfo> fontFiles;
+    List<FileInfo> downloadedFontFiles;
+    FontLoader fontLoader;
+
+    const List<String> fontUrls = <String>[
+      firaSansBoldUrl,
+      firaSansItalicUrl,
+      firaSansRegularUrl,
+      firaSansThinUrl,
+    ];
+
+    setUpAll(() async {
+      downloadedFontFiles = await Future.wait(fontUrls.map(
+        (String url) async {
+          final String generatedCacheKey = cacheKeyFromUrl(url);
+
+          return cacheManager.downloadFile(url, key: generatedCacheKey);
+        },
+      ));
+
+      fontLoader = FontLoader(fontName);
+
+      fontFiles = await DynamicCachedFonts.loadCachedFamily(
+        fontUrls,
+        fontFamily: firaSans,
+        fontLoader: fontLoader,
+      );
+    });
+
+    testWidgets('loads font into cache', (_) async {
+      expect(fontFiles.every((FileInfo file) => file != null), isTrue);
+    });
+
+    testWidgets('loads valid font files', (_) async {
+      for (final FileInfo font in fontFiles) {
+        final FileInfo downloadedFont = downloadedFontFiles[fontFiles.toList().indexOf(font)];
+        expect(font.file.readAsBytesSync(), downloadedFont.file.readAsBytesSync());
+      }
+    });
+
+    testWidgets('loads font into the Flutter Engine', (_) async {
+      // This tests that load() is being called in loadCachedFamily.
+      // A single instance of FontLoader can only be loaded once, so if
+      // StateError is thrown, it means that load() has already been called.
+      expect(fontLoader.load(), throwsStateError);
     });
   });
 
