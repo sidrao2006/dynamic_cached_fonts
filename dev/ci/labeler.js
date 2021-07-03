481787ef28ec16640ed242a79e921adb9ac64a25
Aneesh Rao <sidrao2006@gmail.com>
2021-07-03 22:54:19 +0530

    ci(labeler): Cleanup labeler file by moving commands to a separate file (#121)

---
+++ dev/ci/labeler.js
@@ -0,0 +1,35 @@
+exports.addRevertLabel = async ({ github, context }) => {
+  const { data: pr } = await github.pulls.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.payload.pull_request.number,
+  });
+
+  if (pr.title.includes("Revert")) github.issues.addLabels({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.payload.pull_request.number,
+    labels: ['revert :rewind:'],
+  });
+}
+
+exports.addReleaseLabel = async ({ github, context }) => {
+  const requiredFiles = ['CHANGELOG.md', 'pubspec.yaml', 'example/pubspec.lock'];
+
+  const listFilesOptions = github.pulls.listFiles.endpoint.merge({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.payload.pull_request.number,
+  });
+
+  const listFilesResponse = await github.paginate(listFilesOptions);
+  const changedFiles = listFilesResponse.map(file => file.filename);
+
+  if (requiredFiles.every(file => changedFiles.includes(file)))
+    await github.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.payload.pull_request.number,
+      labels: ['release :tada:'],
+    });
+}
